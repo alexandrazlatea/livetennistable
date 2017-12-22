8b9d8269745ba431ad443a9b5324c01b28d9a265
Zlatea Alexandra <zlatea.alexandra@gmail.com>
2017-12-22 22:07:03 +0100

    change database url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express'),
   bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
+mongoose.connect('mongodb://admin:Test123456@ds131137.mlab.com:31137/livetabletennis');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
